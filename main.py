f2d42fa7e92b7e3516d9c81cdf9478b3cbd2da60
Marcus Gonçalves <nvrrox@gmail.com>
2021-02-18 23:52:10 -0300

    adjust to argparse info

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import argparse
 import datetime
-
 from src import app
 
 if __name__ == '__main__':
@@ -25,27 +24,25 @@ if __name__ == '__main__':
         '-s',
         '--stream',
         type=int,
-        choices=[0, 1],
-        default=0,
+        choices=[-1, 0, 1],
+        default=-1,
         help="""How the app should get data. If this option set to 0,
             then provide a date for -sDate and -eDate""")
 
     parser.add_argument(
         '-sDate',
-        '--startDate',
+        '--start_date',
         type=str,
-        default=str(datetime.datetime.now().strftime('%Y-%m-%d 00:00:00')),
+        default=str(datetime.datetime.now().strftime('%Y%m%d0000')),
         help='When set to 0, the app will get data from this date to -eDate')
 
     parser.add_argument(
         '-eDate',
-        '--endDate',
+        '--end_date',
         type=str,
-        default=str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+        default=str(datetime.datetime.now().strftime('%Y%m%d%H%M')),
         help="""When stream set to 0,
             the app will get data from startDate to this""")
 
     args = parser.parse_args()
-    print(args)
-
     app.serve(args)
